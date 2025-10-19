bce09044aa4167db4d7fb0fee1f094eede6a5442
Chet07-R <178676635+Chet07-R@users.noreply.github.com>
2025-10-19 16:40:11 +0530

    fix issue occuring due to lazy loading in whatsapp

--- Desktop/chetan/personal/Programming/whatsapp-auto/scheduler.py
+++ Desktop/chetan/personal/Programming/whatsapp-auto/scheduler.py
@@ -15,13 +15,36 @@ driver.get("https://web.whatsapp.com/")
 wait=WebDriverWait(driver,100)
 
 # as the target is inside span tag(we are targetting XPath here)
-target='"Di"'
+# target='"Sparsh"'
+
+target='Sparsh'
 message='testing'
 
+search_xpath = '//div[@contenteditable="true"][@data-tab="3"]'
+search_box = wait.until(EC.presence_of_element_located((By.XPATH, search_xpath)))
+
+print(f"Searching for {target}...")
+
+# Clear previous search
+search_box.clear()
+time.sleep(1)
+search_box.send_keys(target)
+time.sleep(2)
+search_box.send_keys(Keys.ENTER)
+time.sleep(3)
+
+
+# Whatsapp uses a thing called lazy loading so i there was a problem in sending messages to older contact so we are using the above code snippet to directly search contact
+
 # general syntax of xpath- XPath=//tagname[@attribute='value']
-contact_path='//span[contains(@title,'+ target +')]'
-contact=wait.until(EC.presence_of_element_located((By.XPATH,contact_path)))
-contact.click()
+# try:
+#     contact_path = f'//span[@title={target}]'
+#     contact = wait.until(EC.presence_of_element_located((By.XPATH, contact_path)))
+# except:
+#     contact_path = f'//span[contains(@title,{target})]'
+#     contact = wait.until(EC.presence_of_element_located((By.XPATH, contact_path)))
+# contact.click()
+
 
 message_box_path='//*[@id="main"]/footer/div[1]/div/span/div/div[2]/div/div[3]/div[1]/p'
 message_box=wait.until(EC.presence_of_element_located((By.XPATH,message_box_path)))
